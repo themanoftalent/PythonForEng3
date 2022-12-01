7fee89fc2bbd22ab5b51ea328243ff3704ba1468
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-01 11:53:01 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+#print("Hello world")
+print("Hello world")
+print('Hello people')
+
+print('I\'m 35 years old')
+
+print("I'm 35 years old")
+message= """ This is about sending ,
+ more messages to my fridns
+ who is outside
+ now in Africa"""
+print(message,end=".")
